4e1239b242f81f832111a5d9a55a329f492d4b87
junaid <junaid@adobe.com>
2014-02-05 09:48:00 -0800

    Reverse list

--- test/LinkedListTest.php
+++ test/LinkedListTest.php
@@ -1,38 +1,38 @@
 <?php
 
 /*
-Copyright (c) 2010, Junaid Najeeb All rights reserved.
+  Copyright (c) 2010, Junaid Najeeb All rights reserved.
 
-Redistribution and use in source and binary forms, with or
-without modification, are permitted provided that the following
-conditions are met:
+  Redistribution and use in source and binary forms, with or
+  without modification, are permitted provided that the following
+  conditions are met:
 
-* Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
+ * Redistributions of source code must retain the above copyright
+  notice, this list of conditions and the following disclaimer.
 
-* Redistributions in binary form must reproduce the above
-copyright notice, this list of conditions and the following
-disclaimer in the documentation and/or other materials provided
-with the distribution.
+ * Redistributions in binary form must reproduce the above
+  copyright notice, this list of conditions and the following
+  disclaimer in the documentation and/or other materials provided
+  with the distribution.
 
-* Neither the name of the origanization nor the names of its
-contributors may be used to endorse or promote products derived
-from this software without specific prior written permission.
+ * Neither the name of the origanization nor the names of its
+  contributors may be used to endorse or promote products derived
+  from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
-CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 require_once(dirname(__FILE__) . '/../library/LinkedList.php');
 
@@ -137,48 +137,63 @@ class LinkedListTest extends PHPUnit_Framework_TestCase {
 		$link_list->insertHead(1);
 		$link_list->insertTail(2);
 	}
-	
+
 	public function testDeleteByValue() {
-		
+
 		$link_list = new LinkedList();
-		
+
 		// delete empty
 		$result = $link_list->deleteByValue(3);
 		$this->assertFalse($result);
-		
+
 		// delete non existing value
 		$link_list->insertHead(1);
 		$result = $link_list->deleteByValue(3);
 		$this->assertFalse($result);
-		
-		
+
+
 		// delete head 
 		$result = $link_list->deleteByValue(1);
 		$this->assertEquals(0, $link_list->size());
-		
-		
+
+
 		// delete tail
 		$link_list->insertHead(1);
 		$link_list->insertTail(2);
 		$link_list->insertTail(3);
-		
+
 		$result = $link_list->deleteByValue(3);
 		$this->assertEquals(2, $link_list->size());
 		$array_list = $link_list->listToArray();
 		$this->assertEquals(1, $array_list[0]);
 		$this->assertEquals(2, $array_list[1]);
-		
+
 		// delete any
 		unset($link_list);
 		$link_list = new LinkedList();
 		$link_list->insertHead(1);
 		$link_list->insertTail(2);
 		$link_list->insertTail(3);
-		
+
 		$result = $link_list->deleteByValue(2);
 		$array_list = $link_list->listToArray();
 		$this->assertEquals(1, $array_list[0]);
 		$this->assertEquals(3, $array_list[1]);
+	}
+
+	public function testReverseList() {
+		
+		$link_list = new LinkedList();
+		$link_list->insertHead(1);
+		$link_list->insertTail(2);
+		$link_list->insertTail(3);
+		
+		$link_list->reverseList();
+		
+		$array_list = $link_list->listToArray();
+		$this->assertEquals(3, $array_list[0]);
+		$this->assertEquals(2, $array_list[1]);
+		$this->assertEquals(1, $array_list[2]);
 		
 	}
 
